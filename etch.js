1032093dfe60cf04fbad14ef403d42248977b927
petery123 <139976625+petery123@users.noreply.github.com>
2024-07-09 23:46:13 +0100

    Add fuctionality to change color on hover
    This was achieved by adding a new class to the class list.

--- etch.js
+++ etch.js
@@ -11,3 +11,9 @@ for (i = 0; i < 16; i++){
 for (i = 0; i < 16; i++){
     container.appendChild(line.cloneNode(true));
 }
+
+container.addEventListener('mouseover', handleMouseOver);
+
+function handleMouseOver(event){
+    console.log(event.target.classList.add("changeBC"));
+}
