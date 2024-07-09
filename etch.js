77f335855ad857ed9ff579ac4d6981c63e42dfbb
petery123 <139976625+petery123@users.noreply.github.com>
2024-07-10 00:04:32 +0100

    Make grid boxes changable to fit set size
    -Button for request for custom number added but no functionality has been added to it

--- etch.js
+++ etch.js
@@ -2,18 +2,23 @@ const container = document.querySelector(".container");
 const line = document.createElement("div");
 line.classList.add("line")
 
-for (i = 0; i < 16; i++){
+const SIDES = 700;
+let size = 100;
+let boxSide = SIDES / size;
+
+for (i = 0; i < size; i++){
     const box = document.createElement("div");
     box.classList.add("box");
+    box.setAttribute("style", `width: ${boxSide}px; height: ${boxSide}px`)
     line.appendChild(box);
 }
 
-for (i = 0; i < 16; i++){
+for (i = 0; i < size; i++){
     container.appendChild(line.cloneNode(true));
 }
 
 container.addEventListener('mouseover', handleMouseOver);
 
 function handleMouseOver(event){
-    console.log(event.target.classList.add("changeBC"));
+    event.target.classList.add("changeBC");
 }
