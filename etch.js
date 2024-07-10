3b5992e573c2c831625350ef36d1b3ab1e913433
petery123 <139976625+petery123@users.noreply.github.com>
2024-07-10 17:11:08 +0100

    Add button fuctionality to alter size and validate
    -It adequately checks if the entry is a number and between 1 and 100

--- etch.js
+++ etch.js
@@ -1,23 +1,28 @@
 const container = document.querySelector(".container");
-const line = document.createElement("div");
-line.classList.add("line")
+const column = document.createElement("div");
+column.classList.add("column")
 
-const SIDES = 700;
+const setSizeBtn = document.querySelector("#setSizeBtn");
+
+const CONTAINER_LENGTH = 700;
 let size = 16;
-let boxSide = SIDES / size;
-
-for (i = 0; i < size; i++){
-    const box = document.createElement("div");
-    box.classList.add("box");
-    box.setAttribute("style", `width: ${boxSide}px; height: ${boxSide}px`)
-    line.appendChild(box);
-}
-
-for (i = 0; i < size; i++){
-    container.appendChild(line.cloneNode(true));
-}
+let boxLength = CONTAINER_LENGTH / size;
 
 container.addEventListener('mouseover', handleMouseOver);
+setSizeBtn.addEventListener('click', handleBtnClick);
+
+function setGrid(){
+    for (i = 0; i < size; i++){
+        const box = document.createElement("div");
+        box.classList.add("box");
+        box.setAttribute("style", `width: ${boxLength}px; height: ${boxLength}px`)
+        column.appendChild(box);
+    }
+    
+    for (i = 0; i < size; i++){
+        container.appendChild(column.cloneNode(true));
+    }
+}
 
 function handleMouseOver(event){
     if (event.target.className === "container") {
@@ -25,3 +30,25 @@ function handleMouseOver(event){
     }
     event.target.classList.add("changeBC");
 }
+
+function handleBtnClick(){
+    let entry = +(prompt("Enter number of rows you want. MAX of 100!"));
+    if (entry === null){
+        return;
+    }
+    if (entry > 100 || entry < 1 || isNaN(entry)){
+        alert("INVALID ENTRY!");
+        return;
+    }
+    size = entry;
+    clearContainerLine();
+    setGrid();
+}
+
+function clearContainerLine(){
+    container.querySelectorAll(".column").forEach(element => element.remove());
+    column.querySelectorAll(".box").forEach(element => element.remove());
+    boxLength = CONTAINER_LENGTH / size;
+}
+
+setGrid();
